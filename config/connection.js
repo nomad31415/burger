4b91a2e68395f43c6e3f6c0f1f1fad95d4419de3
mcatoeff@gmail.com <mcatoeff@gmail.com>
2019-04-23 23:46:49 -0700

    connect?

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,10 @@
 var mysql = require("mysql");
+var connection;
+
+//JAWSDB for Heroku deployment
+if (process.env.NODE_ENV === "production") {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -6,6 +12,6 @@ var connection = mysql.createConnection({
     password: "para55shut",
     database: "burgers_db"
   });
-
+}
 connection.connect();
 module.exports = connection;
